2ce41e52aff9577734359dfe1c8db4aaa34aee80
julio.a.luque <julio.a.luque@gmail.com>
2020-11-15 07:03:15 -0300

    Password encriptado

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,7 +1,7 @@
-INSERT INTO `usuarios` (username, password, enabled, nombre, apellido, email) VALUES ('jluque','12345',1,'Julio','Luque','julio.luque@gmail.com');
-INSERT INTO `usuarios` (username, password, enabled, nombre, apellido, email) VALUES ('andy','42342',1,'Andres','Gutierrez','andres.gutierrez@gmail.com');
-INSERT INTO `usuarios` (username, password, enabled, nombre, apellido, email) VALUES ('zkneo','65745',2,'Martin','Dominguez','martin.dominguez@gmail.com');
-INSERT INTO `usuarios` (username, password, enabled, nombre, apellido, email) VALUES ('admin','00000',1,'boss','manage','bos.manage@gmail.com');
+INSERT INTO `usuarios` (username, password, enabled, nombre, apellido, email) VALUES ('julio','$2a$10$HGsRMPJ5Yto8H69H6AZDiO3NfXVrmxyMUqZON8qonNoxDxd7mor2G',1,'Julio','Luque','julio.luque@gmail.com');
+INSERT INTO `usuarios` (username, password, enabled, nombre, apellido, email) VALUES ('cecii','$2a$10$W13dmsCEM2SHn.94x56wH.wteui1oRVgr3GEV4mkYvuYdgITUH12u',1,'Andres','Gutierrez','andres.gutierrez@gmail.com');
+INSERT INTO `usuarios` (username, password, enabled, nombre, apellido, email) VALUES ('delfi','$2a$10$exw1S0jVyu.LcieldOtRlO03NdMOi/Dna6.cb4jk5tAm.rZouysii',2,'Martin','Dominguez','martin.dominguez@gmail.com');
+INSERT INTO `usuarios` (username, password, enabled, nombre, apellido, email) VALUES ('admin','$2a$10$AwkX9mhQQJz//X1Ob4Rege8PRs7QUIBfMBOByYNAm/zjWGw65uNnS',1,'boss','manage','bos.manage@gmail.com');
 
 INSERT INTO `roles` (nombre) VALUES('ROLE_USER');
 INSERT INTO `roles` (nombre) VALUES('ROLE_ADMIN');
